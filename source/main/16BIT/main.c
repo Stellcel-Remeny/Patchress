9703402304e67f0c5e6cf697cfcad32008290be8
NTSP3 <ntos32bit@gmail.com>
2025-09-06 09:03:19 +0530

    New user select entry thing thing

--- source/main/16BIT/main.c
+++ source/main/16BIT/main.c
@@ -19,10 +19,58 @@
 
 // ---[ Defines ]--- //
 #define status(fmt, ...) status(screen_rows, fmt, ##__VA_ARGS__)
+#define MAX_ENTRIES 100         // Max menu/entry items
 
 // ---[ Global variables ]--- //
 int screen_rows = 0;
 bool animate = true;
+
+// ---[ Functions ]--- //
+void user_select_entry_thing(){
+    int total_num_items = 0, num_entries = 0, num_menus = 0;
+    int key = 0, i = 0;
+    char *menus[100], *entries[100];
+    char current_folder[128] = "RES";
+    bool entry_selected = false;
+
+    while (!entry_selected) {
+        chdir(current_folder);
+        total_num_items = get_entries(menus, entries, ".", MAX_ENTRIES);
+
+        if (total_num_items == 0) {
+            print_page("No items found here to list. Press ESC to go back.");
+            status("  ESC = Go up  F3 = You are a Quitter");
+        } else {
+            num_entries = count_arrays(entries);
+            num_menus = count_arrays(menus);
+
+            i = 0;
+            while (i < num_menus) {
+                print_page("%c: MENU: %s", 'A' + i, menus[i]);
+                i++;
+            }
+
+            i = 0;
+            while (i < num_entries) {
+                print_page("%c: ENTRY: %s", 'A' + num_menus + i, entries[i]);
+                i++;
+            }
+            status("  <A-Z> = Select item  ESC = Go up  F3 = Quit");
+        }
+
+        // User option
+        key = -1;
+        do {
+            key = getch();
+            if (key == 27) status("future...."); // ESC key
+            if (key == 0) quit_check(key);
+            if ((key >= 'A' && key <= 'Z') || (key >= 'a' && key <= 'z')) {
+                if (key >= 'a' && key <= 'z') key = key - 'a' + 'A';   // convert to uppercase
+                status("You pressed: %c", key);
+            }
+        } while (true);
+    }
+}
 
 // ---[ Main ]--- //
 
@@ -34,99 +82,39 @@ int main() {
        return -1;
     }
 
-    bool menu_selected = false;
-    int key, index, total_num_entries, num_entries, num_menus = 0;
+    int key = 0, index = 0;
     char *page;
-    char *menus[MAX_ENTRIES], *entries[MAX_ENTRIES];
-    char current_folder[128] = "RES";
 
     intro(screen_rows, animate);
 
     page1:
-        title("Remeny Patchress [MS-DOS]");
+    title("Remeny Patchress [MS-DOS]");
 
-        delay(100);
-        _settextposition(5, 1);
+    delay(100);
+    _settextposition(5, 1);
 
-        page = "  Welcome to Patchress!\n\n"
-               "   This application contains some utilities.\n\n\n"
-               "   Press any key to continue...";
-        print_page(page);
-        print_page("The Mitochondria is the Powerhouse of the Cell.");
-        status("  ENTER = Continue  F3 = Exit");
+    page = "  Welcome to Patchress!\n\n"
+           "   This application contains some utilities.\n\n\n"
+           "   Press any key to continue...";
+    print_page(page);
+    print_page("The Mitochondria is the Powerhouse of the Cell.");
+    status("  ENTER = Continue  F3 = Exit");
 
-        key = getch();
-        if (key == 13)
-            goto page2;
+    key = -1;
+    while (key != 13 && key != 0) key = getch();
+    
+    if (key == 13)
+        goto page2;
 
-        if (key == 0) { // extended key
-            key = getch();
-            if (key == 61) quit(); // F3 Key
-        }
+    quit_check(key);
 
     // Page 2
     page2:
-        status("Gathering entries...");
-        wipe();
-        page = "  Please select an item from below.\n\n";
+    status("Gathering entries...");
+    wipe();
+    page = "  Please select an item from below.\n\n";
 
-        do {
-            chdir(current_folder);
-            total_num_entries = get_entries(menus, entries, ".", MAX_ENTRIES);
-            delay(100);
-            _settextposition(5, 1);
-            print_page(page);
-
-            if (total_num_entries == 0) {
-                print_page("  No entries found. Please ensure you have the correct folder structure.");
-                status("  ESC = Back  F3 = Exit");
-            } else {
-                for (int i = 0; i < total_num_entries; i++) {
-                    // TODO: Add support for displaying Longer names defined in name=
-                    // TODO: Add support for showing version, author, etc
-                    // TODO: Add support for selecting using Arrow keys
-                    // holy its currently dark here - it was a bundle of sunshine just minutes before! Now omg heavy clouds are covering the sun, guys!
-                    // 8:56am
-
-                    print_page("  %c. %s", 'A' + i, menus[i] ? menus[i] : entries[i]);
-                }
-                num_entries = count_arrays(entries);
-                num_menus = count_arrays(menus);
-                status("  <letter> = Select  ESC = Back  F3 = Exit");
-            }
-            // Get the letter, ESC or F3 key
-            while (true) {
-                key = getch();
-                if (key >= 'A' && key <= 'Z') {
-                    index = key - 'A';
-                } else if (key >= 'a' && key <= 'z') {
-                    index = key - 'a';
-                } else if (key == 0) { // extended key
-                    key = getch();
-                    if (key == 61) quit(); // F3 Key
-                } else {
-                    continue;
-                }
-                // It is expected that entires would be shown first, then menus.
-                // So, we first check if index > number of entries (not num_entries variable).
-                print_page("KEY: %d, TOTAL_ENTRIES: %d, NUMBEROFENTRIES: %d, NUMBEROFMENUS: %d", index, total_num_entries, num_entries, num_menus);
-                if (index < num_entries) {
-                    // An entry was selected
-                    status("You selected entry: %s", entries[index]);
-                    //menu_selected = true;
-                } else {
-                    index = index - num_entries; // This gives us index for menus
-                    if (menus[index]) {
-                        // A menu was selected
-                        //strcpy(current_folder, menus[index]);
-                        menu_selected = false;
-                        //break; // Break inner loop to refresh menu
-                    } else {
-                        status("Invalid selection. Please try again.");
-                    }
-                }
-            }
-        } while (!menu_selected);
-        
+    user_select_entry_thing();
+    
     return 0;
 }
