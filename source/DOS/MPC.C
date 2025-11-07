7afad8132ce5edbe3c128c54d24714f86445476c
NTSP3 <ntos32bit@gmail.com>
2025-11-07 18:01:03 +0530

    Source node rewriting base

---
+++ source/DOS/MPC.C
@@ -0,0 +1,15 @@
+/*
+  Code for 16-bit version of Patchress
+  2025 Remeny
+
+  Compile using Turbo C++ 3.5 compiler
+*/
+
+// Include
+#include <stdio.h>
+#include <conio.h>
+
+// Main
+int main(int argc, char *argv[]) {
+    return 0;
+}
