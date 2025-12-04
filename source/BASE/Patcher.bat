7aa4c37ff0789616c4febecf5c74f4ce9f66aa15
NTSP3 <ntos32bit@gmail.com>
2025-12-04 19:31:46 +0530

    Updated patcher to distinguish between dos/win3x-9x/winnt-xp/vista+

--- source/BASE/Patcher.bat
+++ source/BASE/Patcher.bat
@@ -7,26 +7,32 @@ REM Windows 95/98/ME, or Windows NT.
 REM
 
 if not "%PROCESSOR_ARCHITECTURE%"=="" goto WINNT
-if not "%WINBOOTDIR%"=="" goto WIN9X
+if not "%WINDIR%"=="" goto WIN9X
 goto MSDOS
 
 :WINNT
-if exist %systemroot%\System32\user32.dll (
-    REM NT4 or higher (GUI subsystem present)
-    if not exist patchx32.exe goto PATCH_32_NOT_FOUND
-    start "" "patchx32.exe" %*
-) else (
-    REM NT3.51 or lower
-    echo Your version of Windows NT (NT < 4.0) does not have a dedicated application yet. Sorry!
-)
+REM Windows NT
+ver | find " 6." >nul && goto WINVISTA
+ver | find " 10." >nul && goto WINVISTA
+REM Assume all other NT versions are Windows XP or below. NTVDM will handle this 16bit app.
+if not exist patchg16.exe goto PATCH_GUI16_NOT_FOUND
+patchg16.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+goto END
+
+:WINVISTA
+REM Windows Vista+ - uses 32bit gui app.
+if not exist patchg32.exe goto PATCH_GUI32_NOT_FOUND
+patchg32.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
 goto END
 
 :WIN9X
-if not exist patchx32.exe goto PATCH_32_NOT_FOUND
-start "" "patchx32.exe" %*
+REM Windows 3.1/95/98/ME
+if not exist patchg16.exe goto PATCH_GUI16_NOT_FOUND
+patchg16.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
 goto END
 
 :MSDOS
+REM Pure MS-DOS
 if not exist patchx16.exe goto PATCH_16_NOT_FOUND
 patchx16.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
 goto END
@@ -34,14 +40,21 @@ goto END
 :PATCH_16_NOT_FOUND
 echo.
 echo The file 'patchx16.exe' could not be found in the current directory.
-echo Therefore, ...cannot continue.
+echo Try launching the appropriate application for your system manually.
 echo.
 goto END
 
-:PATCH_32_NOT_FOUND
+:PATCH_GUI16_NOT_FOUND
 echo.
-echo The file 'patchx32.exe' could not be found in the current directory.
-echo Therefore, ...cannot continue.
+echo The file 'patchg16.exe' could not be found in the current directory.
+echo Try launching the appropriate application for your system manually.
+echo.
+goto END
+
+:PATCH_GUI32_NOT_FOUND
+echo.
+echo The file 'patchg32.exe' could not be found in the current directory.
+echo Try launching the appropriate application for your system manually.
 echo.
 goto END
 
