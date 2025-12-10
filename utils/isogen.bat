af108daae4a6970b3e950963973254b57154f79f
NTSP3 <ntos32bit@gmail.com>
2025-12-10 21:08:41 +0530

    Improved compatibility when creating ISO image

--- utils/isogen.bat
+++ utils/isogen.bat
@@ -10,13 +10,30 @@ REM Outputs to: ..\ISO.iso
 REM Input directory is FIXED to ..\BIN for now.
 
 REM Expects:
-REM    - ..\BIN folder    -> containing MultiPatcher output [plus add more stuff to add into iso image]
-REM    - BOOT.IMG in BIN  -> Floppy image - contains BIOS boot code + Stuff for A:\ drive
+REM    - ..\BIN folder          -> containing MultiPatcher output [plus add more stuff to add into iso image]
+REM    - BOOT.IMG in BIN        -> Floppy image - contains BIOS boot code + Stuff for A:\ drive
+REM    - ISOLINUX loader in BIN
 REM
-REM    - MKISOFS.EXE in directory of ISOGEN.BAT
+REM    - XORRISO.EXE and its stuff in directory of ISOGEN.BAT
 
 REM switch to directory of b_tch script
 cd /d "%~dp0"
 
 REM Create bootable image.
-mkisofs -o ..\ISO.iso -b BOOT.IMG -c boot.cat -R -D ..\BIN
+xorriso -as mkisofs ^
+  -iso-level 3 ^
+  -full-iso9660-filenames ^
+  -volid MPC ^
+  -eltorito-boot BOOT/isolinux.bin ^
+  -eltorito-catalog boot.cat ^
+  -no-emul-boot ^
+  -boot-load-size 4 ^
+  -boot-info-table ^
+  -eltorito-alt-boot ^
+  -e BOOT/efi.img ^
+  -no-emul-boot ^
+  -eltorito-platform efi ^
+  -isohybrid-mbr isohdpfx.bin ^
+  -isohybrid-gpt-basdat ^
+  -o ..\ISO.iso ^
+  ..\BIN
